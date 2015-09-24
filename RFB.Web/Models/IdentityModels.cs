3fba9e95ab11c3370704ded66a758402d9cc0ead
unknown <vkanimerakala@hyd-vkanimerkala.alliance-consulting.com>
2015-09-24 17:17:22 +0530

    Just for testing GIT

--- RFB.Web/Models/IdentityModels.cs
+++ RFB.Web/Models/IdentityModels.cs
@@ -14,7 +14,7 @@ namespace RFB.Web.Models
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
-            return userIdentity;
+            return userIdentity;      
         }
     }
 
